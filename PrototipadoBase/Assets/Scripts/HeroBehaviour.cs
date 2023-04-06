c696da7971eadfa5036e47664c3f944d214f5703
ilapadula <ignacio@furthergames.com>
2023-04-05 21:53:24 -0300

    Revert "Merge pull request #1 from ramavm123/HotDashSupreme"
    This reverts commit 3e437802bf0d60b3c05cf89b4ef818a21eb67902, reversing
    changes made to 41205da61385c762ba78e6b2cc6ffb98940014c4.

--- PrototipadoBase/Assets/Scripts/HeroBehaviour.cs
+++ PrototipadoBase/Assets/Scripts/HeroBehaviour.cs
@@ -4,47 +4,56 @@ using UnityEngine;
 
 public class HeroBehaviour : MonoBehaviour
 {
-    public float maxPower = 50f;
-    public float powerMultiplier = 1f;
+    public float velocity = 10f;
+    public int healthPoints = 100;
 
-    private Vector3 startPoint;
-    private Vector3 endPoint;
-    private Vector3 direction;
-    private float distance;
-    private float currentPower;
-    private bool isDragging;
-
-    private Rigidbody rb;
-
-    private void Start()
+    void Update()
     {
-        rb = GetComponent<Rigidbody>();
-        startPoint = transform.position;
+        var speed = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            speed += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            speed += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            speed += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            speed += Vector3.back;
+        }
+
+        if (speed != Vector3.zero)
+        {
+            transform.Translate(speed.normalized * (velocity * Time.deltaTime), Space.World);
+            transform.rotation = Quaternion.LookRotation(speed);
+        }
+
+        if (healthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    private void Update()
+    void OnTriggerEnter(Collider other)
     {
-        if (Input.GetMouseButtonDown(0))
+        if (other.gameObject.CompareTag("Enemy"))
         {
-            startPoint = transform.position;
-            isDragging = true;
+            healthPoints--;
+            Debug.Log($"Health points {healthPoints}");
         }
+    }
 
-        if (isDragging)
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
         {
-            endPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            endPoint.y = 0;
-            direction = (endPoint - startPoint).normalized;
-            distance = Vector3.Distance(startPoint, endPoint);
-
-            currentPower = Mathf.Clamp(distance * powerMultiplier, 0f, maxPower);
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            isDragging = false;
-            rb.AddForce(direction * currentPower, ForceMode.Impulse);
-            currentPower = 0f;
+            healthPoints--;
+            Debug.Log($"Health points {healthPoints}");
         }
     }
 }
