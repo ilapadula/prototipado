fe93574b874aa9ffbf3d8dc314fc28963fb9a43d
ValentinMorcillo <morcillovalen@gmail.com>
2023-04-14 17:09:05 -0300

    Movimiento pj bien hecho

--- PrototipadoBase/Assets/Scripts/HeroBehaviour.cs
+++ PrototipadoBase/Assets/Scripts/HeroBehaviour.cs
@@ -8,48 +8,52 @@ public class HeroBehaviour : MonoBehaviour
     public int healthPoints = 100;
     public LineRenderer lineRenderer;
     public Camera camera;
-    
+    public float friction = 0.95f;
+    private Vector3 speed;
+    private Vector3 direction;
+
     void Update()
     {
-        var speed = Vector3.zero;
         if (Input.GetMouseButtonDown(0))
         {
             // Set the start point of the line
             lineRenderer.SetPosition(0, transform.position);
         }
-        
+
         if (Input.GetMouseButton(0))
         {
             // Get the current mouse position
-            Vector3 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);;
-        
+            Vector3 mousePos = camera.ScreenToWorldPoint(Input.mousePosition); ;
+
             // Set the end point of the line
             lineRenderer.SetPosition(1, mousePos);
-
-            transform.LookAt(mousePos);
+            direction = mousePos - transform.position;
+            direction.y = 0;
+            transform.rotation = Quaternion.LookRotation(direction);
+            speed = Vector3.zero;
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             lineRenderer.SetPosition(1, transform.position);
             // aca podriamos calcular la velocidad en funcion de el vector que da la posicion final del mouse (donde hizo mouse up)
-            
-            transform.Translate(Vector3.forward * velocity * Time.deltaTime);
-            if (transform.position.y > 0 || transform.position.y < 0)
-            {
-                transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
-            }
-            
-             
-            
+
+
+            speed = direction * velocity;
+            Debug.Log($"Speed : {speed}");
+
+
         }
 
         if (speed != Vector3.zero)
         {
             // esto hay que cambiarlo por un sistema que aplique inercia a una velocidad inicial
-            transform.Translate(speed.normalized * (velocity * Time.deltaTime), Space.World);
-            transform.rotation = Quaternion.LookRotation(speed);
+
+            transform.Translate(-speed * Time.deltaTime, Space.World);
+            speed *= friction;
+
         }
+
 
         if (healthPoints <= 0)
         {
